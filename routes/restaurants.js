7e34109e6e3688b7c99f43e4c046105f11c68bab
skim121 <skim29121@gmail.com>
2022-01-15 15:11:49 -0600

    Troubleshooting update fnc

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,7 +5,8 @@ const ctrl = require("../controllers");
 router.get("/profiles/:id/restaurants/new", ctrl.restaurants.newRestaurant);
 router.get("/profiles/:id/restaurants/:id", ctrl.restaurants.show);
 router.post("/profiles/:id/restaurants", ctrl.restaurants.create);
-router.get("/profiles/:id/restaurants/:id/edit", ctrl.restaurants.edit);
+router.put("/restaurants/:id", ctrl.restaurants.update);
 router.delete("/restaurants/:id", ctrl.restaurants.destroy);
+router.get("/profiles/:id/restaurants/:id/edit", ctrl.restaurants.edit);
 
 module.exports = router;
