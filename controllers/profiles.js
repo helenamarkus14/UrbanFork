1b4cc5afb15a02068c31ab37fb928af6d59a4211
skim121 <skim29121@gmail.com>
2022-01-17 02:05:40 -0600

    Lotsss of bootstrap and city filtering added

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -20,7 +20,80 @@ const show = (req, res) => {
 
 };
 
+const austin = (req,res) => {
+    db.Profile.findById(req.params.id, function (err, profile) {
+        const newarray = [];
+        profile.restaurants.forEach(function(r){
+            if(r.city === "Austin"){
+            newarray.push(r);}
+        });
+        if (err) return res.send(err);
+        const context = {profile:profile, restlist: newarray};
+        return res.render("profiles/cities/austin", context);
+});
+};
+
+const boston = (req,res) => {
+    db.Profile.findById(req.params.id, function (err, profile) {
+        const newarray = [];
+        profile.restaurants.forEach(function(r){
+            if(r.city === "Boston"){
+            newarray.push(r);}
+        });
+        if (err) return res.send(err);
+        const context = {profile:profile, restlist: newarray};
+        return res.render("profiles/cities/boston", context);
+});
+};
+
+const boulder = (req,res) => {
+    db.Profile.findById(req.params.id, function (err, profile) {
+        const newarray = [];
+        profile.restaurants.forEach(function(r){
+            if(r.city === "Boulder"){
+            newarray.push(r);}
+        });
+        if (err) return res.send(err);
+        const context = {profile:profile, restlist: newarray};
+        return res.render("profiles/cities/boulder", context);
+});
+};
+
+const chicago = (req,res) => {
+    db.Profile.findById(req.params.id, function (err, profile) {
+        const newarray = [];
+        profile.restaurants.forEach(function(r){
+            if(r.city === "Chicago"){
+            newarray.push(r);}
+        });
+        if (err) return res.send(err);
+        const context = {profile:profile, restlist: newarray};
+        return res.render("profiles/cities/chicago", context);
+});
+};
+
+const nyc = (req,res) => {
+    db.Profile.findById(req.params.id, function (err, profile) {
+        const newarray = [];
+        profile.restaurants.forEach(function(r){
+            if(r.city === "NYC"){
+            newarray.push(r);}
+        });
+        if (err) return res.send(err);
+        const context = {profile:profile, restlist: newarray};
+        return res.render("profiles/cities/nyc", context);
+});
+};
+
+
+
 module.exports = {
     idx,
     show,
+    austin,
+    boston,
+    boulder,
+    chicago,
+    nyc,
+
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,5 +3,10 @@ const ctrl = require("../controllers");
 
 router.get("/", ctrl.profiles.idx);
 router.get("/:id", ctrl.profiles.show);
+router.get("/:id/austin", ctrl.profiles.austin);
+router.get("/:id/boston", ctrl.profiles.boston);
+router.get("/:id/boulder", ctrl.profiles.boulder);
+router.get("/:id/chicago", ctrl.profiles.chicago);
+router.get("/:id/nyc", ctrl.profiles.nyc);
 
 module.exports = router;
